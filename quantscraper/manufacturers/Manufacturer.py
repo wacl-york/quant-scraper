54f3b81aa69ec983416a861d2e9c0669634e2f6d
Stuart Lacy <stuart.lacy@gmail.com>
2020-04-29 10:45:35 +0100

    preprocessing script uses a separate JSON file

--- quantscraper/manufacturers/Manufacturer.py
+++ quantscraper/manufacturers/Manufacturer.py
@@ -188,7 +188,6 @@ class Manufacturer(ABC):
         cols = cfg.get(self.name, "columns_to_validate").split(",")
         labels = cfg.get(self.name, "column_labels").split(",")
         scales = cfg.get(self.name, "scaling_factors").split(",")
-        analysis = cfg.get(self.name, "columns_to_preprocess").split(",")
 
         # Ensure have the same number of values
         lengths = [len(cols), len(labels), len(scales)]
@@ -208,7 +207,6 @@ class Manufacturer(ABC):
                 "raw_label": cols[i],
                 "clean_label": labels[i],
                 "scale": scaling_factor,
-                "included_analysis": labels[i] in analysis,
             }
             self.measurands.append(entry)
 
