9d8a9306299eb3673db61f5650320b1e3b7fa223
Stuart Lacy <stuart.lacy@gmail.com>
2020-05-14 18:07:28 +0100

    device list and configuration files are hardcoded
    Initially, the location of the config file was an argument to both of
    the scripts. However, now that the program is going to be deployed in an
    entirely automated fashion, rather than interactive, it is unlikely that
    we will need the flexibility of having the choice of config files.
    Any changes to the software's configuration can be implemented through
    releases or patches.
    
    In addition the location of the devices.json file that defines the relationship
    and properties of the manufacturers and their devices in the study is also
    hardcoded into both scripts.
    In future, this information will likely be stored in a database rather
    than a file.
    
    Finally, minor changes have been made to the unit tests, so that there
    is a test_<x>.py file for each module when necessary, rather than
    grouping tests by shared functionality.

---
+++ tests/utils.py
@@ -0,0 +1,40 @@
+"""
+    tests/utils.py
+    ~~~~~~~~~~~~~~
+
+    Utility functions for unit tests.
+"""
+from unittest.mock import Mock
+import datetime
+
+def build_mock_today(year, month, day):
+    class MockDate(datetime.date):
+        @classmethod
+        def today(cls):
+            return cls(year, month, day)
+
+    return MockDate
+
+
+
+def build_mock_response(
+    status=200, content="CONTENT", json_data=None, raise_for_status=None, text=None,
+):
+    """
+       Helper function to build mock response. Taken from:
+       https://gist.github.com/evansde77/45467f5a7af84d2a2d34f3fcb357449c
+       """
+    mock_resp = Mock()
+    # mock raise_for_status call w/optional error
+    if raise_for_status is not None:
+        mock_resp.raise_for_status = Mock()
+        mock_resp.raise_for_status.side_effect = raise_for_status
+    # set status code and content
+    mock_resp.status_code = status
+    mock_resp.content = content
+    mock_resp.text = text
+    # add json data if provided
+    if json_data is not None:
+        mock_resp.json = Mock(return_value=json_data)
+    # mock_post = Mock(post=Mock(return_value=mock_resp))
+    return mock_resp
