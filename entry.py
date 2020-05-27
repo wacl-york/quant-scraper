9629f08feecc98cd9906df0425cfca251970482c
Stuart Lacy <stuart.lacy@gmail.com>
2020-05-27 16:35:17 +0100

    Added a script to send summary emails using SES

--- entry.py
+++ entry.py
@@ -13,25 +13,32 @@
 
 import argparse
 import subprocess
+from datetime import date, timedelta
 
 
 def main():
     email_fn = "email.html"
     args = parse_args()
 
+    # Default date to yesterday
+    if args.date is None:
+        parse_date = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+    else:
+        parse_date = args.date
+
     # Base calls
     scrape_call = ["quant_scrape", "--save-raw", "--save-clean", "--html", email_fn]
     preprocess_call = ["quant_preprocess"]
+    email_call = ["python", "send_email.py", "--file", email_fn]
+
+    # Always pass in date
+    scrape_call.extend(["--start", parse_date, "--end", parse_date])
+    preprocess_call.extend(["--date", parse_date])
+    email_call.extend(["--date", parse_date])
 
     # Add arguments if passed in
-    if args.date is not None:
-        scrape_call.extend(["--start", args.date, "--end", args.date])
-        preprocess_call.extend(["--date", args.date])
-
     if args.scrape_devices is not None:
-        scrape_call.append("--devices")
-        for dev in args.scrape_devices:
-            scrape_call.append(dev)
+        scrape_call.extend(["--devices", *args.scrape_devices])
 
     if args.upload_raw:
         scrape_call.append("--upload-raw")
@@ -40,17 +47,21 @@ def main():
         scrape_call.append("--upload-clean")
 
     if args.preprocess_devices is not None:
-        preprocess_call.append("--devices")
-        for dev in args.preprocess_devices:
-            preprocess_call.append(dev)
+        preprocess_call.extend(["--devices", *args.preprocess_devices])
 
     if args.upload_preprocess:
         preprocess_call.append("--upload")
 
+    print("Calling quant_scrape with call: {}".format(scrape_call))
     subprocess.run(scrape_call)
+    print("Calling quant_preprocess with call: {}".format(preprocess_call))
     subprocess.run(preprocess_call)
 
-    # TODO Add emailing HTML table here
+    # Only send email if have provided recipients
+    if args.recipients is not None:
+        email_call.extend(["--recipients", *args.recipients])
+        print("Calling send_email with call: {}".format(email_call))
+        subprocess.run(email_call)
 
 
 def parse_args():
@@ -98,6 +109,13 @@ def parse_args():
         "--upload-preprocess",
         action="store_true",
         help="Uploads pre-processed data to Google Drive.",
+    )
+
+    parser.add_argument(
+        "--recipients",
+        metavar="EMAIL@DOMAIN",
+        nargs="+",
+        help="The recipients to send the email to. If not provided, then no email is sent.",
     )
 
     args = parser.parse_args()
