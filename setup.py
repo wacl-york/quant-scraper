0b9249243a130fa596b55f7563ecc0dad9ebec72
Stuart Lacy <stuart.lacy@york.ac.uk>
2020-06-22 13:46:58 +0100

    Updated release version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="quantscraper",
-    version="0.2.1",
+    version="0.3.0",
     description="Scrapes data for the QUANT project.",
     long_description=readme(),  # NB: Only used if upload to PyPi
     url="https://github.com/wacl-york/QUANTscraper",
