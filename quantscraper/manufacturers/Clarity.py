916ff8296fe726f854834f655ea0bfce3f6575fc
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-06-10 09:05:07 +0100

    Scaffolding

---
+++ quantscraper/manufacturers/Clarity.py
@@ -0,0 +1,198 @@
+"""
+    quantscraper.manufacturers.Clarity.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Concrete implementation of Manufacturer, representing the Clarity air
+    quality instrumentation device manufacturer.
+"""
+
+from datetime import datetime, time
+import json
+import os
+import requests as re
+import pandas as pd
+from quantscraper.manufacturers.Manufacturer import Manufacturer
+from quantscraper.utils import LoginError, DataDownloadError, DataParseError
+
+
+class Clarity(Manufacturer):
+    """
+    Inherits attributes and methods from Manufacturer along with providing
+    implementations of:
+        - connect()
+        - scrape_device()
+        - parse_to_csv()
+    """
+
+    name = "Clarity"
+
+    def __init__(self, cfg, fields):
+        """
+        Sets up object with parameters needed to scrape data.
+
+        Args:
+            - cfg (dict): Keyword-argument properties set in the Manufacturer's
+                'properties' attribute.
+            - fields (list): List of dicts detailing the measurands available
+                for this manufacturer and their properties.
+
+        Returns:
+            None
+        """
+        self.session = None
+
+        # Authentication
+        self.base_url = "https://clarity-data-api.clarity.io/v1"
+
+        super().__init__(cfg, fields)
+
+    def connect(self):
+        """
+        Verifies the API token allows for authentication.
+
+        The instance attribute 'session' stores a handle to the connection,
+        holding any generated cookies and the history of requests.
+
+        Args:
+            - None.
+
+        Returns:
+            None, although a handle to the connection is stored in the instance
+            attribute 'session'.
+        """
+        # TODO Adapt for Clarity
+        self.session = re.Session()
+        url_to_call = "{base}/stations".format(base=self.base_url)
+
+        try:
+            result = self.session.get(url_to_call)
+            result.raise_for_status()
+        except re.exceptions.HTTPError as ex:
+            raise LoginError(
+                "Cannot test authentication.\n{}".format(str(ex))
+            ) from None
+        except re.exceptions.ConnectionError as ex:
+            raise DataDownloadError(
+                "Connection error when testing authentication.\n{}".format(str(ex))
+            ) from None
+
+        if result.text == "AUTHENTICATION FAILED":
+            self.session.close()
+            raise LoginError("Login failed")
+
+    def log_device_status(self, device_id):
+        """
+        Scrapes information about a device's operating condition.
+
+        This method does nothing for Clarity, since the devices' calibration
+        settings are already logged in the raw data.
+
+        Args:
+            - device_id (str): The ID used by the website to refer to the
+                device.
+
+        Returns:
+            A dict of keyword-value parameters.
+        """
+        params = {}
+        return params
+
+    def scrape_device(self, device_id, start, end):
+        """
+        Downloads the data for a given device from the website.
+
+        This just requires a single API GET request with the appropriate params.
+        The raw data is held in the 'Data' attribute of the response JSON.
+
+        Args:
+            - device_id (str): The ID used by the website to refer to the
+                device.
+            - start (date): The start of the scraping window.
+            - end (date): The end of the scraping window.
+
+        Returns:
+            The data stored in a hierarchical format comprising dicts and lists.
+            At the top level is a list of timepoints, represented as a dict
+            containing measurements that are stored in a list of dicts in the
+            'Channels' attribute.
+        """
+        # TODO Adapt for Clarity
+        # Convert start and end times into required format of
+        # YYYY-mm-ddTHH:mm:ss
+        # Clarity API uses [closed, open] intervals, so set start time as midnight of
+        # the start day, and end day as 23:59:59 of end day
+        start_dt = datetime.combine(start, time.min)
+        end_dt = datetime.combine(end, time.max)
+        start_fmt = start_dt.strftime("%Y-%m-%dT%H:%M:%S")
+        end_fmt = end_dt.strftime("%Y-%m-%dT%H:%M:%S")
+
+        url_to_call = "{url}/{start}/{end}/{device}".format(
+            url=self.data_url, start=start_fmt, end=end_fmt, device=device_id
+        )
+        try:
+            result = self.session.get(url_to_call)
+            result.raise_for_status()
+        except re.exceptions.HTTPError as ex:
+            raise DataDownloadError(
+                "Cannot download data.\n{}".format(str(ex))
+            ) from None
+        except re.exceptions.ConnectionError as ex:
+            raise DataDownloadError(
+                "Connection error when downloading data.\n{}".format(str(ex))
+            ) from None
+
+        try:
+            data = result.json()
+        except (json.decoder.JSONDecodeError, TypeError):
+            raise DataDownloadError("No 'Data' attribute in downloaded json.") from None
+
+        return data
+
+    def parse_to_csv(self, raw_data):
+        """
+        Parses the raw data into a 2D list format.
+
+        Since the raw data is already in a hierarchical format, it is easier to
+        read it into Pandas as a long data frame and then pivot to wide, rather
+        than do this manually in base Python.
+
+        Args:
+            - raw_data (dict): The data stored in a hierarchical format comprising dicts and lists.
+                At the top level is a list of timepoints, represented as a dict
+                containing measurements that are stored in a list of dicts in the
+                'Channels' attribute.
+
+        Returns:
+            A 2D list representing the data in a tabular format, so that each
+            row corresponds to a unique time-point and each column holds a
+            measurand.
+        """
+        # TODO Adapt for Clarity
+        clean_data = []
+        for timepoint in raw_data:
+            try:
+                for channel in timepoint["Channels"]:
+                    try:
+                        clean_data.append(
+                            {
+                                "Timestamp": timepoint["TBTimestamp"],
+                                "measurand": channel["SensorLabel"],
+                                "value": channel["Scaled"],
+                            }
+                        )
+                    except KeyError:
+                        pass  # If don't have the Scaled or SensorLabel
+                        # properties then skip to next measurement
+            except KeyError:
+                pass  # If don't have Channels object skip to next timepoint
+        df = pd.DataFrame(clean_data)
+        try:
+            df_wide = df.pivot_table(
+                index="Timestamp", columns="measurand", values="value", fill_value=""
+            ).reset_index()
+        except KeyError:
+            raise DataParseError("Unable to pivot long to wide.")
+
+        df_list = [df_wide.columns.tolist()] + df_wide.values.tolist()
+
+        return df_list
