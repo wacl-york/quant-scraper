be23afec10ead68503458e2b183de306cd6ffd47
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-06-14 17:30:37 +0100

    Working scraping routine
    The only outstanding task is to add unit tests.

--- quantscraper/factories.py
+++ quantscraper/factories.py
@@ -9,6 +9,7 @@ import logging
 from quantscraper.manufacturers.Aeroqual import Aeroqual
 from quantscraper.manufacturers.AQMesh import AQMesh
 from quantscraper.manufacturers.Zephyr import Zephyr
+from quantscraper.manufacturers.Vortex import Vortex
 from quantscraper.manufacturers.MyQuantAQ import MyQuantAQ
 from quantscraper.manufacturers.PurpleAir import PurpleAir
 from quantscraper.manufacturers.AURN import AURN
@@ -33,6 +34,7 @@ def manufacturer_factory(config):
         "Zephyr": Zephyr,
         "QuantAQ": MyQuantAQ,
         "PurpleAir": PurpleAir,
+        "Vortex": Vortex,
         "AURN": AURN,
     }
     try:
