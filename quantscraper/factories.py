22a37429baab3b0c877af07bb9e5e9d719ee9268
Stuart Lacy <stuart.lacy@york.ac.uk>
2021-06-18 15:13:44 +0100

    Working scraper

--- quantscraper/factories.py
+++ quantscraper/factories.py
@@ -8,6 +8,7 @@
 import logging
 from quantscraper.manufacturers.Aeroqual import Aeroqual
 from quantscraper.manufacturers.AQMesh import AQMesh
+from quantscraper.manufacturers.Oizom import Oizom
 from quantscraper.manufacturers.Zephyr import Zephyr
 from quantscraper.manufacturers.MyQuantAQ import MyQuantAQ
 from quantscraper.manufacturers.PurpleAir import PurpleAir
@@ -32,6 +33,7 @@ def manufacturer_factory(config):
         "AQMesh": AQMesh,
         "Zephyr": Zephyr,
         "QuantAQ": MyQuantAQ,
+        "Oizom": Oizom,
         "PurpleAir": PurpleAir,
         "AURN": AURN,
     }
